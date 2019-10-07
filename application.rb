6b6403a0c0f1484a402709e7210b760d16add118
github username <github email address>
2019-10-07 17:20:28 -0500

    Done.

--- application.rb
+++ application.rb
@@ -1,9 +1,14 @@
-class Application
+require 'sinatra'
+class Application < Sinatra::Base
 
-  def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+  # def call(env)
+  #   resp = Rack::Response.new
+  #   resp.write "Hello, World"
+  #   resp.finish
+  # end
+
+  get '/:name' do 
+    "Hello, my name is"
   end
 
 end
